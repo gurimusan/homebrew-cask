ca4d1fa9ed9a0ef25e505facbd08649917e39251
松下永壽 <gurimusan@matsushitaeihisashi-no-MacBook-Pro.local>
2014-05-07 21:30:32 +0900

    add my casks

---
+++ Casks/totalspaces2.rb
@@ -0,0 +1,7 @@
+class Totalspaces2 < Cask
+  url 'http://downloads.binaryage.com/TotalSpaces2-2.1.10.zip'
+  homepage 'http://totalspaces.binaryage.com/'
+  version '2.1.10'
+  sha256 '8bf24a7afbde0cdb560b40abd8ab69584621ca6de59026553f007a0da7b4d443'
+  install 'TotalSpaces2.pkg'
+end
